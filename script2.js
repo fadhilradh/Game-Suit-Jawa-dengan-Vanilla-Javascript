0186711262ce0775c32c33809d7171d5e4f5f5e0
Fadhil Radhian <fadhilradhian@gmail.com>
2021-07-08 21:13:41 +0700

    change layout

--- script2.js
+++ script2.js
@@ -1,54 +1,62 @@
-function cariPilihanKomp()  {
+function cariPilihanKomp() {
     const pilihanKomp = Math.random();
-	if (pilihanKomp < 0.34) return `gajah`;
-	if (pilihanKomp >= 0.34 && pilihanKomp <= 0.67) return `semut`;
-	return `orang`;
+    if (pilihanKomp < 0.34) return `batu`;
+    if (pilihanKomp >= 0.34 && pilihanKomp <= 0.67) return `gunting`;
+    return `kertas`;
 }
 
 const dapatHasilSuit = (pilihanPlayer, pilihanKomp) => {
-    if (pilihanPlayer == pilihanKomp) return 'HASILNYA SERI!'; 
-    if (pilihanPlayer == 'gajah') return (pilihanKomp == 'orang') ? 'ANDA MENANG !!' : 'ANDA KALAH !!';
-    if (pilihanPlayer == 'orang') return (pilihanKomp == 'gajah') ? 'ANDA KALAH !!' : 'ANDA MENANG !!'; 
-    if (pilihanPlayer == 'semut') return (pilihanKomp == 'orang') ? 'ANDA KALAH !!' : 'ANDA MENANG !!';
-}
-    
-const klikGajah = document.querySelector('.gajah');
-const klikOrang = document.querySelector('.orang');
-const klikSemut = document.querySelector('.semut');
-const info = document.querySelector('.info');
+    if (pilihanPlayer == pilihanKomp) return "HASILNYA SERI!";
+    if (pilihanPlayer == "batu")
+        return pilihanKomp == "gunting" ? "ANDA MENANG !!" : "ANDA KALAH !!";
+    if (pilihanPlayer == "gunting")
+        return pilihanKomp == "batu" ? "ANDA KALAH !!" : "ANDA MENANG !!";
+    if (pilihanPlayer == "kertas")
+        return pilihanKomp == "gunting" ? "ANDA KALAH !!" : "ANDA MENANG !!";
+};
+
+const klikBatu = document.querySelector(".batu");
+const klikGunting = document.querySelector(".gunting");
+const klikKertas = document.querySelector(".kertas");
+const info = document.querySelector(".info");
 const gambarKomputer = document.querySelector(".img-komputer");
 
-
-const ambilSemuaGambarPlayer = document.querySelectorAll('li img');
+const ambilSemuaGambarPlayer = document.querySelectorAll("li img");
 
 const acakGambar = () => {
-    const gambar = ['gajah', 'semut', 'orang'];
+    const gambar = ["batu", "gunting", "kertas"];
     const waktuMulai = new Date().getTime();
     let i = 0;
-    setInterval(function() {
+    setInterval(function () {
         if (new Date().getTime() - waktuMulai > 1500) {
             clearInterval;
-            return
+            return;
         }
-        gambarKomputer.setAttribute('src', 'img/' + gambar[i++] +'.png')
+        gambarKomputer.setAttribute("src", "assets/" + gambar[i++] + ".png");
         if (i == gambar.length) i = 0;
-    }, 100)
-}
+    }, 100);
+};
 
-ambilSemuaGambarPlayer.forEach(function(klikGambarApa) {
-    klikGambarApa.addEventListener('click', function() {
+ambilSemuaGambarPlayer.forEach(function (klikGambarApa) {
+    klikGambarApa.addEventListener("click", function () {
         const pilihanPlayer = klikGambarApa.className;
         const pilihanKomp = cariPilihanKomp();
         const hasil = dapatHasilSuit(pilihanPlayer, pilihanKomp);
 
         acakGambar();
 
-        setTimeout(function() {
+        setTimeout(function () {
             info.innerText = hasil;
-            gambarKomputer.setAttribute('src', `img/${pilihanKomp}.png`); 
+            gambarKomputer.setAttribute("src", `assets/${pilihanKomp}.png`);
         }, 1500);
-        info.innerText = '';
-        });
+        info.innerText = "";
+    });
 });
 
+//function expression
 
+const luasPersegi = function luasPersegi(a, b) {
+    return a * b;
+};
+
+console.log("Luas persegi adalah " + luasPersegi(5, 5));
